07a4a23daa10cda85e9044e427e44f89b80a1a52
Lucas Albano <lucas@albano.us>
2020-08-27 09:11:31 -0400

    added initial files 2

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return(
+            <div>
+                My App Component, Lucas
+            </div>
+        );
+    }
+}
+export default App
